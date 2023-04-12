870d1997fb6eae2d574b99abf265c969cef245bf
sashka <che-4ILL@yandex.ru>
2023-04-12 20:21:46 +0300

    всё ещё работает

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,7 @@
 class RegistrationsController < ApplicationController
-    	def new
+	def new
 		@user = User.new
-        end
+	end
 	def create
 		@user = User.new(user_params)
 		if @user.save
@@ -15,4 +15,5 @@ class RegistrationsController < ApplicationController
 	end
 	private def user_params
 		params.require(:user).permit(:number, :password, :password_confirmation)
+	end
 end
