74dd817d382b542691fdbb1f5a44a219ef61ead2
sashka <che-4ILL@yandex.ru>
2023-06-16 12:35:44 +0300

    Добавление корзины

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :line_items
+  resources :carts
   get "sing_up", to: "registrations#new"
   post "sing_up", to: "registrations#create"
   get "sing_in", to: "sessions#new"
